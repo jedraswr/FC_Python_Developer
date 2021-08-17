796cc5e1f3b6b86ae91292fa673448ab8fce2a0d
Wojciech Jdrasik <wojciech.jedrasik@tutamail.com>
2021-08-17 10:37:56 +0200

    Z poprawka na brak pliku prognozy

--- weather/weather.py
+++ weather/weather.py
@@ -5,8 +5,8 @@ import datetime
 import os
 import time
 import json
-baza_prognoz = {}                       # robocza baza danych wybranych elementów prognoz
-marker_i = 0                            # znacznik zapobiegający pętleniu się importu jeśli nie można odnaleźć prognozy
+baza_prognoz = {}       # robocza baza danych wybranych elementów prognoz
+marker_importu = 0      # znacznik zapobiegający pętleniu się importu jeśli nie można odnaleźć prognozy
 
 # Wczytywanie założeń:
 klucz_api = sys.argv[1]
@@ -18,15 +18,32 @@ if czy_data == 3:
     data_zadana = datetime.datetime.strptime(data, "%Y-%m-%d")
 else:
     data_zadana = dzisiaj + datetime.timedelta(days=1)
-data_zadana = time.mktime(data_zadana.timetuple())  # data zadana to data oczekiwanej prognozy (podana, lub nazajutrz)
+data_zadana = time.mktime(data_zadana.timetuple())  # data oczekiwanej prognozy
 
 # Sprawdzanie granicznego horyzontu prognozy:
 ile_dni = int((data_zadana - data_dzisiaj) / (24 * 60 * 60))
-if ile_dni > 16 or ile_dni < 0:
+if ile_dni > 15 or ile_dni < 0:                     # okres prognoz = dziś + 15 dni
     print("Nie wiem - długość prognozy to od 0 do 16 dni do przodu.")
     exit()
 
+# Obsluga API:
+
+def import_z_api():
+    if marker_importu == 0:
+        url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+        querystring = {"q":"warsaw,pl","lat":"35","lon":"139","cnt":"16","units":"metric or imperial"}
+        headers = {
+            'x-rapidapi-key': klucz_api,
+            'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
+            }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        with open("prognoza.json", "w", encoding="utf-8") as file:
+            json.dump(response.json(), file)
+
 # Tworzenie bazy prognoz z pliku json:
+if not os.path.isfile("prognoza.json"):
+    import_z_api()
+    marker_importu = 1
 
 def tworzenie_bazy():
     prognoza_pelna = open("prognoza.json", "r", encoding="utf-8")
@@ -54,26 +71,15 @@ def wydruk_prognozy():
     if data_zadana in baza_prognoz:
         print(baza_prognoz[data_zadana])
         exit()
+    elif marker_importu == 1:
+        print("Nie wiem - nie znalazłem prognozy z tej daty")
+        exit()
     else:
         import_z_api()
 
 wydruk_prognozy()
 
-# Obsluga API:
-def import_z_api():
-    if marker_i == 0:
-        url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
-        querystring = {"q":"warsaw,pl","lat":"35","lon":"139","cnt":"16","units":"metric or imperial"}
-        headers = {
-            'x-rapidapi-key': klucz_api,
-            'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
-            }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        with open("prognoza.json", "w") as file:
-            json.dump(response.json(), file)
-
 # Wyszukiwanie prognozy ze zaktualizowanych zasobów:
 tworzenie_bazy()
-marker_i = 1
 wydruk_prognozy()
 
